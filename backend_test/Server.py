48c08e74b30a5a027bdee795375fbcf9ecef7e98
junxian428 <wengyin777@gmail.com>
2023-07-10 10:00:13 +0100

    first commit

--- backend_test/Server.py
+++ backend_test/Server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import eventlet
 
 app = Flask(__name__, template_folder='templates')
 
-#app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
@@ -16,6 +16,16 @@ def handle_message(message):
     response = f"You said: {message}"
     emit('message', response)
 
+    # Spawn a new greenlet to send messages to the client
+    eventlet.spawn(send_message_to_client)
+
+def send_message_to_client():
+    while True:
+        # Send a message to the client
+        socketio.send("This is a server-initiated message")
+
+        # Delay between messages
+        eventlet.sleep(5)
+
 if __name__ == '__main__':
     socketio.run(app, host='localhost', port=8765)
-s
